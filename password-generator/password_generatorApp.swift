1291e69c17befe2074cc1e683950b5048fa12ccb
Vitor Scheffer <vitorscheffer61@gmail.com>
2024-03-01 10:49:47 -0300

    Initial Commit

---
+++ password-generator/password_generatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  password_generatorApp.swift
+//  password-generator
+//
+//  Created by Vitor Boff on 01/03/24.
+//
+
+import SwiftUI
+
+@main
+struct password_generatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ password-generatorUITests/password_generatorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  password_generatorUITestsLaunchTests.swift
+//  password-generatorUITests
+//
+//  Created by Vitor Boff on 01/03/24.
+//
+
+import XCTest
+
+final class password_generatorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
