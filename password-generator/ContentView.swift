f01630e8fd7ac369c99fe8b9513a17fe67b4ab42
Vitor Scheffer <vitorscheffer61@gmail.com>
2024-03-01 12:42:48 -0300

    feat: Adiciona base do projeto

--- password-generator/ContentView.swift
+++ password-generator/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var speed = 10.0
+    
+    private func didTapGeneratePassword() {
+        print(Int(speed))
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Color(red: 243/255, green: 243/255, blue: 255/255).ignoresSafeArea()
+
+            VStack(spacing: 20) {
+                Image("logo", bundle: .main)
+                    .imageScale(.large)
+                    .padding(.bottom, 60)
+                
+                Text("\(Int(speed)) caracteres")
+                    .font(.title)
+                    .foregroundStyle(Color.black)
+                
+                Slider(
+                    value: $speed,
+                    in: 6...18
+                )
+                .padding(16)
+                .background(.white)
+                .clipShape(.buttonBorder)
+                
+                Button(action: didTapGeneratePassword, label: {
+                    Text("Gerar senha")
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity)
+                        .padding(.vertical, 12)
+                })
+                .background(.blue)
+                .clipShape(RoundedRectangle(cornerRadius: 8))
+            }
+            .padding(.horizontal, 20)
         }
-        .padding()
     }
 }
 
